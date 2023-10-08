e6fe98d0ce2b0c8972a9a3ead5b5632d31eff6e0
Kishorè Shanto <tinykishore@icloud.com>
2023-10-08 20:35:47 +0600

    Added Footer svelte component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,16 @@
+<footer class=" fixed bottom-0 w-full md:flex mx-auto py-2 px-32 z-50
+justify-between sm:justify-between align-middle items-center text-sm text-zinc-600 shadow-sm
+rounded-t-lg bg-gradient-to-l from-yellow-100 to-amber-50">
+    <div id="left-side" class="flex flex-row gap-2 gap-x-6 flex-wrap">
+        <p class="select-none cursor-default">&copy; 2023 Agri-Inn, Inc.</p>
+        <a href="/" class="hover:text-amber-700 hover:underline">Terms</a>
+        <a href="/" class="hover:text-amber-700 hover:underline">Sitemap</a>
+        <a href="/" class="hover:text-amber-700 hover:underline">Privacy</a>
+        <a href="/" class="hover:text-amber-700 hover:underline">Your Privacy Choices</a>
+    </div>
+
+    <div class="text-gray-900 font-bold flex-row flex gap-x-6">
+        <a href="/" class="hover:underline">English (US)</a>
+        <a href="/" class="hover:underline">Support and Resource</a>
+    </div>
+</footer>
