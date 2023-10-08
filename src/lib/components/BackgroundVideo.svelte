878ffb8e2bc43b89c0e725dda605b897dcf4c50e
Kishorè Shanto <tinykishore@icloud.com>
2023-10-08 20:36:05 +0600

    Added BackgroundVideo svelte component

---
+++ src/lib/components/BackgroundVideo.svelte
@@ -0,0 +1,5 @@
+<div class="absolute top-0 left-0 w-full h-screen -z-10 select-none no-scroll">
+    <video class="brightness-75 absolute object-cover top-0  min-w-full min-h-screen -z-[1000] select-none" autoplay muted loop playsinline>
+          <source type="video/mp4" src="/src/lib/assets/videos/landing-page-background-video.mp4"/>
+    </video>
+</div>
