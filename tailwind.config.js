68941d5c42337141eb35e4015cbd03042b976189
Kishorè Shanto <tinykishore@icloud.com>
2023-10-08 20:31:29 +0600

    Updated Inter and -apple-system font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    fontFamily: {
+      'sans': ['-apple-system', 'BlinkMacSystemFont', "Inter", 'Helvetica Neue', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'],
+      'mono': ['SFMono-Regular', 'JetBrains Mono',],
+    },
     extend: {}
   },
   plugins: []
 };
+
+
