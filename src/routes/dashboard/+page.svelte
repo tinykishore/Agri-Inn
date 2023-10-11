1da8994c2da1ba0a594cf82401505ed1d5510829
Kishorè Shanto <tinykishore@icloud.com>
2023-10-12 00:05:46 +0600

    Added `/dashboard`

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import currentNavigation from "$lib/stores/currentNavigation";
+    import DashboardNavigation from "$lib/components/dynamicNavigations/DashboardNavigation.svelte";
+    import username from "$lib/stores/username";
+    currentNavigation.set(DashboardNavigation);
+    export let data;
+    username.set(data.username);
+</script>
+
+<main class="mt-12">
+	{data.username} logged in
+</main>
+
