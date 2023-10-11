e0f137962d6efe86c4ed214e13865bb0a99e29b1
Kishorè Shanto <tinykishore@icloud.com>
2023-10-11 21:51:48 +0600

    Added `auth/SignInAPI` Sign In API

---
+++ src/routes/API/v1/auth/SignInAPI/+server.ts
@@ -0,0 +1,63 @@
+// Login POST API
+
+import {getUserByEmail, getUserByUsername} from "$lib/server/database";
+import consoleLog, {LEVEL} from "$lib/server/log";
+import bcrypt from "bcrypt";
+import {generateToken} from "$lib/server/utility";
+
+export const POST = async ({request}: any) => {
+    consoleLog("SignInAPI REQUEST Received", LEVEL.OK)
+    // Server Should receive a Credential JSON object
+    const credentials: SignInCredentials = await request.json();
+    const key: string = credentials.key;
+    const password: string = credentials.password;
+
+    // Check if the key is username or email
+    const keyIsEmail: boolean = key.includes('@');
+
+    // Define authResult variable, which will be used to determine if the user is authenticated
+    let authResult;
+    if (keyIsEmail) {
+        consoleLog("SignInAPI API REQUEST: key is email", LEVEL.OK)
+        // If the key is an email, find the user by email
+        authResult = await getUserByEmail(key);
+    } else {
+        consoleLog("SignInAPI API REQUEST: key is username", LEVEL.OK);
+        // If the key is a username, find the user by username
+        authResult = await getUserByUsername(key);
+    }
+
+    // If the user is not found, return a 401 status code
+    if (!authResult) {
+        consoleLog("SignInAPI RESPONSE: status 401", LEVEL.ERROR);
+        return new Response(null, {status: 401});
+    }
+
+    // If the user is found, check if the password is correct
+    if (await bcrypt.compare(password, authResult.credentials["password_hash"])) {
+        consoleLog("SignInAPI RESPONSE: Password Matched", LEVEL.OK);
+        consoleLog("SignInAPI RESPONSE: Sending response with cookie headers", LEVEL.OK);
+        // If the password is correct, generate a token and send it back to the client
+        const token: string = generateToken({key: credentials.key});
+        // Add Cookie Headers to the response with CORS Headers
+        // Redirect to the dashboard
+        return new Response(
+            JSON.stringify({
+                name: authResult.name,
+                email: authResult.email,
+                token: token
+            }),
+            {
+                status: 200,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+                    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                    'Set-Cookie': `sessionID=${token}; Path=/; Secure; HttpOnly; SameSite=Strict; Max-Age=21600`
+                }
+            });
+    } else {
+        consoleLog("SignInAPI RESPONSE: Password Mismatch", LEVEL.ERROR)
+        return new Response(null, {status: 401});
+    }
+}
