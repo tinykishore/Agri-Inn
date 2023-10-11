fb34311ae52a8caeb7470bd3341919dca870615b
Kishorè Shanto <tinykishore@icloud.com>
2023-10-11 21:51:07 +0600

    Added `global.ts` global variables

---
+++ src/lib/server/globals.ts
@@ -0,0 +1,23 @@
+/**
+ * Enumeration representing user roles.
+ * @enum {number}
+ */
+export enum USER_ROLE {
+    /**
+     * Administrator role.
+     * @type {number}
+     */
+    ADMIN = 0,
+
+    /**
+     * Regular user role.
+     * @type {number}
+     */
+    USER = 1,
+
+    /**
+     * Owner role.
+     * @type {number}
+     */
+    OWNER = 2
+}
