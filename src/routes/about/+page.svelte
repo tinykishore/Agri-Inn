d1e1a0be10c283c0e375447fb58ca685509f96e5
Kishorè Shanto <tinykishore@icloud.com>
2023-10-11 20:43:13 +0600

    /about and /search page created

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import currentNavigation from "$lib/stores/currentNavigation";
+    import AboutNavigation from "$lib/components/dynamicNavigations/AboutNavigation.svelte";
+    currentNavigation.set(AboutNavigation);
+
+</script>
+
+
+<main class="h-[1000px]">
+	Hello
+</main>
