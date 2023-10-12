b656e151b4e70eeb005a748cda336acb6e84218c
Kishorè Shanto <tinykishore@icloud.com>
2023-10-12 09:29:16 +0600

    Created `/farms` route

--- src/routes/farms/+page.svelte
+++ src/routes/farms/+page.svelte
@@ -1,15 +1,28 @@
-<script>
-    export let data
+<script lang="ts">
+    import {onMount} from "svelte";
+
+    // export let data
+
+    let farms: any;
+    onMount(async () => {
+        const response = await fetch('/API/v1/farms/GetFarmsAPI');
+        farms = await response.json();
+    });
 
 </script>
 
 <main class="my-24">
-	<div class="flex flex-col my-12 mx-24 gap-8">
-		<h1>{data.email}</h1>
-		<h1>{data.username}</h1>
-		<h1>{data.user_role}</h1>
-		<h1>{data.full_name}</h1>
+	{#if farms === undefined}
+		<div class="flex flex-col my-12 mx-24 gap-8">
+			<h1>Loading...</h1>
+		</div>
+	{:else}
+		<div class="flex flex-col my-12 mx-24 gap-8">
+			{#each farms as farm}
+				<a href="/farms/{farm.uid}">{farm.farm_name}</a>
+			{/each}
+		</div>
+	{/if}
 
-	</div>
 
 </main>
