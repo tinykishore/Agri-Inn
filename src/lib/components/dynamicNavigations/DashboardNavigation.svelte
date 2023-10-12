5c46bc9c01d82af61daddde6dc40d83206ca739e
Kishorè Shanto <tinykishore@icloud.com>
2023-10-12 08:50:39 +0600

    Added Spinner to Dashboard, Handled user details store

--- src/lib/components/dynamicNavigations/DashboardNavigation.svelte
+++ src/lib/components/dynamicNavigations/DashboardNavigation.svelte
@@ -4,6 +4,7 @@
     import {fade} from "svelte/transition";
     import {onMount} from "svelte";
     import username from "$lib/stores/username";
+    import navigationUserDetails from "$lib/stores/navigationUserDetails";
 
     let _username: string | null;
     let full_name: string;
@@ -15,26 +16,56 @@
     })
 
     onMount(async () => {
-        const response = await fetch('/API/v1/dynamicNavbar/GetUserDetails', {
-            method: 'POST',
-            body: JSON.stringify({username: _username}),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        const data = await response.json();
-        full_name = data.full_name;
-        email = data.email;
-        image = data.profile_picture;
+        navigationUserDetails.subscribe(value => {
+            full_name = value.full_name;
+            email = value.email;
+            image = value.profile_picture;
+        })
+        if (full_name === "" && email === "" && image === "") {
+            const response = await fetch('/API/v1/dynamicNavbar/GetUserDetails', {
+                method: 'POST',
+                body: JSON.stringify({username: _username}),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const data = await response.json();
+            full_name = data.full_name;
+            email = data.email;
+            image = data.profile_picture;
+			navigationUserDetails.set({
+				full_name: full_name,
+				email: email,
+				profile_picture: image
+			})
+        }
     })
+
+    const onSignOutClick = () => {
+        document.getElementById('close_image')?.classList.add('hidden');
+        document.getElementById('sign_out_spinner')?.classList.remove('hidden');
+    }
 </script>
 
 <div class="grid grid-cols-3 items-center">
 	<div class="flex justify-start gap-2">
 		<form action="?/SignOut" method="POST" use:enhance>
 			<button class="px-3 py-2 flex text-sm items-center gap-0.5 align-middle rounded-full font-bold border border-yellow-800 hover:bg-red-100 text-yellow-950 transition-all duration-300" name="log_out"
-					type="submit"><span
-					class="block h-5 w-5"><img alt="cancel_icon" src={cancel_icon}></span>
+					type="submit" on:click={onSignOutClick}>
+				<span id="close_image" class="block h-5 w-5 mr-1">
+					<img alt="cancel_icon" src={cancel_icon}>
+				</span>
+				<span id="sign_out_spinner" class="hidden">
+					<svg class="animate-spin h-5 w-5 mr-1 text-yellow-800" xmlns="http://www.w3.org/2000/svg"
+						 fill="none"
+						 viewBox="0 0 24 24">
+						<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+								stroke-width="4"></circle>
+						<path class="opacity-75" fill="currentColor"
+							  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0
+							  3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+					</svg>
+				</span>
 				Sign Out
 			</button>
 		</form>
@@ -57,8 +88,7 @@
 
 	<div class="flex justify-end gap-2 text-sm">
 
-			{#if full_name === undefined && email === undefined && image === undefined}
-			<!--	Loading svg -->
+			{#if full_name === "" && email === "" && image === ""}
 			<svg class="animate-spin  h-[30px] w-[30px] mr-1 text-yellow-800" xmlns="http://www.w3.org/2000/svg" fill="none"
 				 viewBox="0 0 24 24">
 				<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
