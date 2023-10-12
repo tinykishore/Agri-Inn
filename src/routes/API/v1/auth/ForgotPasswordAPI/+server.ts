a99db7a0c9705106c087ff580f0be6eb0529db9d
Swarnab Biswas <mysbincognito@gmail.com>
2023-10-12 15:58:36 +0600

    Implemented experimental forgot password

--- src/routes/API/v1/auth/ForgotPasswordAPI/+server.ts
+++ src/routes/API/v1/auth/ForgotPasswordAPI/+server.ts
@@ -1,6 +1,32 @@
 // Forgot Password POST API
 
+/* MUST READ
+
+    New packages required: nodemailer, crypto-js
+    change connectToMongo() and closeMongoConnection() to your own functions
+    change the transporter object to your own email provider
+    And so on.
+
+
+ */
+
 import consoleLog, {LEVEL} from "$lib/server/log";
+import {fail} from "@sveltejs/kit";
+import nodemailer from "nodemailer";
+import CryptoJS from "crypto-js";
+
+let transporter = nodemailer.createTransport({
+    host: 'SMTP.EXAMPLE-HOST.COM',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'example@email.com',
+        pass: 'APP_PASS'
+    },
+    tls: {
+        ciphers: 'SSLv3'
+    }
+});
 
 export const POST = async ({request}: any) => {
     consoleLog("ForgotPasswordAPI REQUEST Received", LEVEL.OK)
@@ -13,14 +39,78 @@ export const POST = async ({request}: any) => {
     }
     consoleLog("ForgotPasswordAPI API REQUEST: Email is " + email, LEVEL.OK);
 
-    // Step:1 Check if the email is valid
+    if (!validateEmail(email)) return fail(401, {email, emailInvalid: true});
 
-    // Step:2 Check if the email exists in the database
+    try {
 
-    // Step:3 If so, Generate a token and send it to the user's email
+        const db = await connectToMongo(); // Connect to Mongo
+        const collection = db.collection('users'); // Define collection
+        const query = {email: email};
+        const success = await collection.findOne(query);
 
-    // Step:4 If not, return a 404 status code
+        if (success) {
 
-    return new Response(JSON.stringify({body: 'Email sent.'}), {status: 200});
+            const jumble = jumbleGenerator(); // Generate a unique code for the user
 
+            const collection = db.collection('lost-tokens'); // Define collection then send the code bound with the email
+            const query = {
+                email: email,
+                token: jumble,
+                issueTime: new Date().getTime()
+            };
+            const success = await collection.insertOne(query);
+
+            if (success) {
+
+                const link = 'https://example.com/lost-password/' + jumble; // Change link
+
+                await sendMail(email, link);
+
+                return new Response(JSON.stringify({body: 'Email sent.'}), {status: 200});
+
+            } else {
+                return fail(401, {email, databaseError: true});
+            }
+
+        } else {
+
+            // Return fail
+
+        }
+
+    } catch (error) {
+        console.log(error);
+    } finally {
+        await closeMongoConnection(); // Close Mongo connection
+    }
+}
+
+async function sendMail(to: string, link: string) {
+
+    let html = '<h3>Password reset</h3>' + '<h3>Your link: ' + link + '</h3>' +
+        '<p>Click the link above to change your password.</p>' +
+        '<p>Link expires in 30 minutes.</p>' +
+        '<p>If you did not request a password reset, please ignore this email.</p>' +
+        '<p>Regards, Joshboi</p>' +
+        '<p style="color: red">Note: Do not share the link with anyone. Not even site admins.</p>';
+
+    const info = await transporter.sendMail({
+        from: 'Joshboi <password-reset@joshboi.xyz>',
+        to: to,
+        subject: 'Password Reset',
+        html: html
+    });
+}
+
+function jumbleGenerator(): string | null {
+    const jumble = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    const hash = CryptoJS.MD5(jumble).toString();
+    const jumbleArray = hash.match(/.{1,4}/g);
+    if (jumbleArray) return jumbleArray.join('-');
+    return null;
+}
+
+function validateEmail(email: string): boolean {
+    const emailRegex = new RegExp("^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$");
+    return emailRegex.test(email);
 }
