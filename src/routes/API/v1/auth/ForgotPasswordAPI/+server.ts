27814551047a261895504821124d3eb4564f9b19
Kishorè Shanto <tinykishore@icloud.com>
2023-10-12 20:26:57 +0600

    Added ForgotPasswordAPI

--- src/routes/API/v1/auth/ForgotPasswordAPI/+server.ts
+++ src/routes/API/v1/auth/ForgotPasswordAPI/+server.ts
@@ -1,27 +1,18 @@
 // Forgot Password POST API
 
-/* MUST READ
-
-    New packages required: nodemailer, crypto-js
-    change connectToMongo() and closeMongoConnection() to your own functions
-    change the transporter object to your own email provider
-    And so on.
-
-
- */
-
 import consoleLog, {LEVEL} from "$lib/server/log";
-import {fail} from "@sveltejs/kit";
 import nodemailer from "nodemailer";
 import CryptoJS from "crypto-js";
+import {GMAIL_APP_PASS} from "$env/static/private";
+import {insertResetPasswordToken} from "$lib/server/database";
 
 let transporter = nodemailer.createTransport({
-    host: 'SMTP.EXAMPLE-HOST.COM',
+    host: 'smtp.gmail.com',
     port: 465,
     secure: true,
     auth: {
-        user: 'example@email.com',
-        pass: 'APP_PASS'
+        user: 'tinykishore@gmail.com',
+        pass: GMAIL_APP_PASS
     },
     tls: {
         ciphers: 'SSLv3'
@@ -29,88 +20,83 @@ let transporter = nodemailer.createTransport({
 });
 
 export const POST = async ({request}: any) => {
-    consoleLog("ForgotPasswordAPI REQUEST Received", LEVEL.OK)
+    consoleLog("ForgotPasswordAPI REQUEST Received", LEVEL.OK);
     // Server Should receive an email JSON object. Extract the email from the request body
     const email = (await request.json()).email;
-
-    // Check if the email is null, undefined, or empty
-    if (email === null || email === undefined || email === '') {
-        return new Response(JSON.stringify({body: 'Email is required.'}), {status: 400})
-    }
     consoleLog("ForgotPasswordAPI API REQUEST: Email is " + email, LEVEL.OK);
 
-    if (!validateEmail(email)) return fail(401, {email, emailInvalid: true});
+    // FIXME: Email verification will be moved to client side
+    // // Check if the email is null, undefined, or empty
+    // if (email === null || email === undefined || email === '') {
+    //     return new Response(null, {status: 404});
+    // }
+    // consoleLog("ForgotPasswordAPI API REQUEST: Email is " + email, LEVEL.OK);
 
-    try {
 
-        const db = await connectToMongo(); // Connect to Mongo
-        const collection = db.collection('users'); // Define collection
-        const query = {email: email};
-        const success = await collection.findOne(query);
+    // FIXME: Email verification will be moved to client side
+    // // Check if the email exists in the database
+    // const emailExists = await getUserByEmail(email);
+    //
+    // // If the email exists, generate a token and send it to the user
+    // if (emailExists) {
+    // consoleLog("ForgotPasswordAPI API REQUEST: Email exists", LEVEL.OK);
 
-        if (success) {
+    // Generate a token
+    consoleLog("ForgotPasswordAPI API REQUEST: Generating token...", LEVEL.OK);
+    const reset_token: string | null = resetTokenGenerator(); // Generate a unique code for the user
 
-            const jumble = jumbleGenerator(); // Generate a unique code for the user
-
-            const collection = db.collection('lost-tokens'); // Define collection then send the code bound with the email
-            const query = {
-                email: email,
-                token: jumble,
-                issueTime: new Date().getTime()
-            };
-            const success = await collection.insertOne(query);
-
-            if (success) {
-
-                const link = 'https://example.com/lost-password/' + jumble; // Change link
-
-                await sendMail(email, link);
-
-                return new Response(JSON.stringify({body: 'Email sent.'}), {status: 200});
-
-            } else {
-                return fail(401, {email, databaseError: true});
-            }
-
-        } else {
-
-            // Return fail
-
-        }
-
-    } catch (error) {
-        console.log(error);
-    } finally {
-        await closeMongoConnection(); // Close Mongo connection
+    // Check if the token is null. If it is, return an error
+    if (reset_token === null) {
+        consoleLog("ForgotPasswordAPI API REQUEST: Token generation failed", LEVEL.ERROR);
+        return new Response(null, {status: 500});
     }
+    consoleLog("ForgotPasswordAPI API REQUEST: Token generated: " + reset_token, LEVEL.OK);
+
+    // Insert the token into the database
+    const successInsertion: boolean = await insertResetPasswordToken(email, reset_token);
+
+    if (successInsertion) {
+        consoleLog("ForgotPasswordAPI API REQUEST: Database updated", LEVEL.OK);
+        // TODO: Change the link to the actual link
+        const link = 'https://example.com/lost-password/' + reset_token;
+        await sendMail(email, link);
+        return new Response(null, {status: 200});
+    } else {
+        consoleLog("ForgotPasswordAPI API REQUEST: Email does not exist", LEVEL.ERROR);
+        return new Response(null, {status: 500});
+    }
+
+    // } else return new Response(null, {status: 404});
 }
 
 async function sendMail(to: string, link: string) {
+    let html = `
+  <div style="font-family: 'Helvetica Neue', sans-serif; text-align: center;">
+	<div style="background-color: #f5f5f5; max-width: 500px; margin: 0 auto; padding: 20px; border-radius: 15px; box-shadow: 0 8px 12px rgba(0, 0, 0, 0.2);">
+		<h2 style="color: #3D220C; font-size: 24px; font-weight: 700">Password Reset</h2>
+		<p style="font-size: 16px;font-weight: 700">Click the link below to change your password</p>
+		<p style="font-size: 16px;">If you did not request a password reset, please ignore this email</p>
+		<a href="${link}" style="display: inline-block; padding: 10px 20px; background-color: #3D220C; color: #ffffff; text-decoration: none; font-size: 18px; border-radius: 10px; margin: 10px 0 10px 0; font-weight: 700">Reset Password</a>
+		<p style="color: #e53e3e; font-size: 14px; font-weight: bold">Note: Do not share the link with anyone, not even site admins</p>
+		<p style="font-size: 16px;color: #737373; font-weight: 600; margin-top: 50px">Regards</p>
+		<img src="https://raw.githubusercontent.com/tinykishore/Agri-Inn/development/src/lib/assets/icons/logo.jpg" alt="Logo" style="width: 100px;">
+	</div>
+</div>
+`;
 
-    let html = '<h3>Password reset</h3>' + '<h3>Your link: ' + link + '</h3>' +
-        '<p>Click the link above to change your password.</p>' +
-        '<p>Link expires in 30 minutes.</p>' +
-        '<p>If you did not request a password reset, please ignore this email.</p>' +
-        '<p>Regards, Joshboi</p>' +
-        '<p style="color: red">Note: Do not share the link with anyone. Not even site admins.</p>';
-
+// You can send this HTML in your email.
     const info = await transporter.sendMail({
-        from: 'Joshboi <password-reset@joshboi.xyz>',
+        from: 'Agri-Inn',
         to: to,
-        subject: 'Password Reset',
+        subject: 'Password Reset | Agri-Inn',
         html: html
     });
 }
 
-function jumbleGenerator(): string | null {
-    const jumble = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    const hash = CryptoJS.MD5(jumble).toString();
-    const jumbleArray = hash.match(/.{1,4}/g);
-    if (jumbleArray) return jumbleArray.join('-');
+function resetTokenGenerator(): string | null {
+    const key = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    const keyHash = CryptoJS.MD5(key).toString();
+    const tokenArray = keyHash.match(/.{1,4}/g);
+    if (tokenArray) return tokenArray.join('-');
     return null;
-}
-
-function validateEmail(email: string): boolean {
-    const emailRegex = new RegExp("^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$");
-    return emailRegex.test(email);
 }
