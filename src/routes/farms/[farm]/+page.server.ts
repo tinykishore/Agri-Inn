847296a1c1a2be85e0366a7fba7051b7e10938c7
Kishorè Shanto <tinykishore@icloud.com>
2023-10-12 10:23:57 +0600

    Added `/farms/[farm]` authorization logic

---
+++ src/routes/farms/[farm]/+page.server.ts
@@ -0,0 +1,31 @@
+import {redirect} from "@sveltejs/kit";
+import jwt from "jsonwebtoken";
+import {JWT_SECRET} from "$env/static/private";
+
+export const load = async ({cookies, url}: any) => {
+    // Get cookie value "sessionID"
+    const token = cookies.get('sessionID');
+
+    // If the cookie is not found, redirect to sign-in page
+    // Because the user is not authenticated
+    if (!token) throw redirect(307, "/sign-in");
+
+    // If the cookie is found, verify the JWT
+    const authenticated: any = jwt.verify(token, JWT_SECRET);
+    // If the JWT is invalid, redirect to sign-in page
+    if (!authenticated) {
+        throw redirect(307, "/sign-in");
+    }
+
+    const urlPath = url.pathname.split("/");
+    const farm = urlPath[urlPath.length - 1];
+
+    // If the JWT is valid, return the username
+    return {
+        full_name: authenticated.full_name,
+        username: authenticated.username,
+        email: authenticated.email,
+        user_role: authenticated.user_role,
+        farm_uid: farm
+    }
+}
