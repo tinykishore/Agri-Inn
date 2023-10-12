27db1491173e9c6e1a625975625bbcd24be3fdd8
Kishorè Shanto <tinykishore@icloud.com>
2023-10-12 08:54:01 +0600

    Added `/farms` route

---
+++ src/routes/farms/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let data
+
+</script>
+
+<main class="my-24">
+	<div class="flex flex-col my-12 mx-24 gap-8">
+		<h1>{data.email}</h1>
+		<h1>{data.username}</h1>
+		<h1>{data.user_role}</h1>
+		<h1>{data.full_name}</h1>
+
+	</div>
+
+</main>
