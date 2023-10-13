aa257b35ed5c6c7e5a10a1beb5fb91e913975435
Kishorè Shanto <tinykishore@icloud.com>
2023-10-13 22:36:34 +0600

    Created cache handler for `/forum`
    Signed-off-by: Kishorè Shanto <tinykishore@icloud.com>

--- src/routes/forum/+page.svelte
+++ src/routes/forum/+page.svelte
@@ -1,25 +1,35 @@
 <script lang="ts">
-
     import {onMount} from "svelte";
     import UserCache from "$lib/stores/UserCache";
+    import {isUserCacheValid} from "../../globals";
 
-    let postBody: string = "";
+    export let data;
+
     let postTitle: string = "";
+    let postBody: string = "";
     let postAuthor: string = "";
-
-    UserCache.subscribe((user) => {
-        postAuthor = user.username;
-    });
-
     let posts: any = [];
+
     export let onPostSubmit = async () => {
-        let post:Posts = {
-            post: postBody,
+        // Validation Check
+        if (postTitle == "" || postBody == "") {
+            alert("Post Title and Post Body cannot be empty");
+            return;
+        }
+
+        // Get Author
+        UserCache.subscribe(value => {
+            postAuthor = value.username;
+        });
+
+        let post: Post = {
             title: postTitle,
+            post: postBody,
             author: postAuthor,
             timestamp: Date.now(),
             likes: 0
         }
+
         const response = await fetch('/API/v1/forum/InsertPostAPI', {
             method: 'POST',
             body: JSON.stringify(post),
@@ -27,10 +37,32 @@
                 'Content-Type': 'application/json'
             }
         });
+
+        if (response.ok) {
+            alert("Post Submitted");
+            const response = await fetch('/API/v1/forum/GetAllPostAPI');
+            posts = await response.json();
+        } else {
+            alert("Post Submission Failed");
+        }
     }
+
     onMount(async () => {
-        const getAllposts = await fetch('/API/v1/forum/getAllPostAPI');
-        posts = await getAllposts.json();
+        // Update the user cache if it is not valid
+        if (!isUserCacheValid()) {
+            UserCache.update(value => {
+                value.full_name = data.full_name;
+                value.email = data.email;
+                value.profile_picture = data.profile_picture;
+                value.username = data.username;
+                value.role = data.user_role;
+                return value;
+            });
+        }
+
+        // Get all posts via API GetAllPostAPI
+        const response = await fetch('/API/v1/forum/GetAllPostAPI');
+        posts = await response.json();
     });
 
 </script>
@@ -42,6 +74,7 @@
 
     <div>
         <h1>Posts</h1>
+
         <ul>
             {#each posts as post}
                 <li>{post.author}</li>
@@ -52,8 +85,8 @@
                 <button>Like</button>
                 <button>Comment</button>
                 <button>Save</button>
-
             {/each}
         </ul>
-        </div>
+
+    </div>
 </main>
