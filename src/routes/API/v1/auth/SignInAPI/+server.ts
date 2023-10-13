51c746b4f44e89c9fe8c2757cf353bd01ee5685c
Kishorè Shanto <tinykishore@icloud.com>
2023-10-13 10:10:06 +0600

    Modified SignInAPI response

--- src/routes/API/v1/auth/SignInAPI/+server.ts
+++ src/routes/API/v1/auth/SignInAPI/+server.ts
@@ -48,15 +48,18 @@ export const POST = async ({request}: any): Promise<Response> => {
             full_name: authResult["full_name"],
             username: authResult.credentials["username"],
             email: authResult.credentials["email"],
+            profile_picture: authResult["profile_picture"],
             user_role: authResult["role"]
         });
         // Add Cookie Headers to the response with CORS Headers
         // Redirect to the dashboard
         return new Response(
             JSON.stringify({
-                name: authResult.name,
-                email: authResult.email,
-                token: token
+                full_name: authResult["full_name"],
+                username: authResult.credentials["username"],
+                email: authResult.credentials["email"],
+                profile_picture: authResult["profile_picture"],
+                user_role: authResult["role"]
             }),
             {
                 status: 200,
