eb687cc0453268e0bb1c57e852610a0a49ef7657
Swarnab Biswas <mysbincognito@gmail.com>
2023-10-14 15:30:10 +0600

    Experimental forgot password captcha

--- src/routes/sign-in/forgot-password/+page.svelte
+++ src/routes/sign-in/forgot-password/+page.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
 
+    import {Turnstile} from "svelte-turnstile";
+
+    let siteKey = '1x00000000000000000000AA';
+
     let email: string;
 
     const handleSubmit = async () => {
+        const turnstile_input = document.getElementsByName("cf-turnstile-response")[0] as HTMLInputElement;
+        const turnstile_token = turnstile_input.value;
         const res = await fetch('/API/v1/auth/ForgotPasswordAPI', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ email })
+            body: JSON.stringify({email, turnstile_token})
         });
 
         if (res.ok) {
@@ -27,7 +33,9 @@
 
     <form on:submit|preventDefault={handleSubmit}>
         <label for="email">Email</label>
-        <input type="email" id="email" name="email" required bind:value={email} />
+        <input type="email" id="email" name="email" required bind:value={email}/>
+
+        <Turnstile siteKey={siteKey}/>
 
         <button type="submit">Send Reset Link</button>
     </form>
