65298f099846abd82c5542614e4dfc0013fb7d5a
Kishorè Shanto <tinykishore@icloud.com>
2023-10-15 00:26:43 +0600

    Fixed Vercel
    Signed-off-by: Kishorè Shanto <tinykishore@icloud.com>

--- src/lib/components/BackgroundVideo.svelte
+++ src/lib/components/BackgroundVideo.svelte
@@ -1,10 +1,7 @@
-<script lang="ts">
-    import bg_video from "$lib/assets/videos/landing-page-background-video.mp4";
-
-</script>
+<script lang="ts"></script>
 
 <div class="absolute top-0 left-0 w-full h-screen -z-10 select-none no-scroll">
     <video class="brightness-75 absolute object-cover top-0  min-w-full min-h-screen -z-[1000] select-none" autoplay muted loop playsinline>
-          <source type="video/mp4" src={bg_video}/>
+        <source src="/src/lib/assets/videos/landing-page-background-video.mp4" type="video/mp4"/>
     </video>
 </div>
