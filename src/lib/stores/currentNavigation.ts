a936effdad6442c9696de74593b67ca97b46df19
Kishorè Shanto <tinykishore@icloud.com>
2023-10-17 01:27:29 +0600

    Created new store for Navigation Bar
    Signed-off-by: Kishorè Shanto <tinykishore@icloud.com>

--- src/lib/stores/currentNavigation.ts
+++ src/lib/stores/currentNavigation.ts
@@ -1,4 +1,7 @@
 import {writable} from 'svelte/store';
 import DefaultNavigation from "$lib/components/dynamicNavigations/DefaultNavigation.svelte";
 import DashboardNavigation from "$lib/components/dynamicNavigations/DashboardNavigation.svelte";
+
 export default writable(DefaultNavigation || DashboardNavigation || null);
+
+export const uid = writable(null);
