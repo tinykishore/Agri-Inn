f2167109fa2b32f71e2c2d4d05534820cb4fbe92
Kishorè Shanto <tinykishore@icloud.com>
2023-10-19 23:02:51 +0600

    Updated UI
    Signed-off-by: Kishorè Shanto <tinykishore@icloud.com>

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -25,24 +25,54 @@
 	<meta name="description" content="Dashboard">
 </svelte:head>
 
-<main class="mt-12">
-	{data._id} logged in
+<main class="my-20 mx-32">
+
+	<div class="grid-cols-3 grid gap-4">
+		<div class="col-span-2 bg-amber-100 w-full p-4 rounded-2xl">
+			User Greetings
+
+			{userCache?.full_name}
+			{userCache?.email}
+			{userCache?.username}
+			{userCache?.profile_picture}
+			{userCache?.user_role}
+		</div>
+		<div class="bg-orange-200 p-4 rounded-2xl flex flex-col gap-2">
+			<div class="flex flex-col gap-1">
+				<h1 class="text-xl font-bold antialiased text-end text-amber-900">Quick Links</h1>
+				<h1 class="mb-2 text-sm font-bold antialiased text-end bg-gradient-to-bl from-amber-900 to-amber-700 text-transparent bg-clip-text">
+					Suggested From Your Activity
+				</h1>
+			</div>
+
+			<a class="rounded-2xl bg-zinc-50 p-4" href="/farms">
+				<h1 class="text-xl font-bold antialiased text-amber-900">Farms</h1>
+				<h1 class="text-sm font-bold antialiased text-amber-900">Manage your farms</h1>
+			</a>
+
+			<a class="rounded-2xl bg-zinc-50 p-4" href="/forum">
+				<h1 class="text-xl font-bold antialiased text-amber-900">Forum</h1>
+				<h1 class="text-sm font-bold antialiased text-amber-900">Join the community</h1>
+			</a>
+
+			<a class="rounded-2xl bg-zinc-50 p-4" href="/marketplace">
+				<h1 class="text-xl font-bold antialiased text-amber-900">Marketplace</h1>
+				<h1 class="text-sm font-bold antialiased text-amber-900">Buy and sell your crops</h1>
+			</a>
+
+
+		</div>
+	</div>
 	<div class="bg-amber-100 rounded-2xl shadow-xl">
-		{userCache?.full_name}
-		{userCache?.email}
-		{userCache?.username}
-		{userCache?.profile_picture}
-		{userCache?.user_role}
+
 	</div>
 	<h1>Sitemap</h1>
 	<div class="flex gap-4 bg-amber-100 rounded-xl">
-		<a href="/farms">Farms</a>
-		<a href="/forum">Forum</a>
-		<!--		FIXME: Sketchy-->
+
+	<!--		FIXME: Sketchy-->
 		{#if userCache?.user_role === USER_ROLE.OWNER}
 			<a href="/loan">Loan</a>
 		{/if}
-		<a href="/news">News</a>
 		<a href="/marketplace">Marketplace</a>
 	</div>
 </main>
