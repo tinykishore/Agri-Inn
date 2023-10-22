6ab6e1d846f230d8468931259354250a0107e72e
Kishorè Shanto <tinykishore@icloud.com>
2023-10-22 17:47:27 +0600

    Refactored
    Signed-off-by: Kishorè Shanto <tinykishore@icloud.com>

--- src/routes/forum/[id]/+page.server.ts
+++ src/routes/forum/[id]/+page.server.ts
@@ -4,28 +4,27 @@ import {JWT_SECRET} from "$env/static/private";
 
 export const load = async ({cookies, url}: any) => {
     // Get cookie value "sessionID"
-    const token = cookies.get('sessionID');
+    const sessionID = cookies.get('sessionID');
 
     // If the cookie is not found, redirect to sign-in page
     // Because the user is not authenticated
-    if (!token) throw redirect(307, "/sign-in");
+    if (!sessionID) throw redirect(307, "/sign-in");
 
     // If the cookie is found, verify the JWT
     let authenticated: any;
     try {
-        authenticated = jwt.verify(token, JWT_SECRET);
+        authenticated = jwt.verify(sessionID, JWT_SECRET);
     } catch (e) {
         console.log(e);
         throw redirect(307, "/sign-in");
     }
 
     const urlPath = url.pathname.split("/");
-    const post = urlPath[urlPath.length - 1];
-    console.log(post);
+    const postObjectID = urlPath[urlPath.length - 1];
 
     // If the JWT is valid, return the username
     return {
-        _id: authenticated._id,
-        post_uid: post
+        userObjectID: authenticated.userObjectID,
+        postObjectID: postObjectID
     }
 }
