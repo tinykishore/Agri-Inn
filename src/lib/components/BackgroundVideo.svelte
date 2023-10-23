282ca28d1af8df22539d6bfe02aa977d176fd9ee
Swarnab Biswas <mysbincognito@gmail.com>
2023-10-23 22:31:46 +0600

    Added to component

--- src/lib/components/BackgroundVideo.svelte
+++ src/lib/components/BackgroundVideo.svelte
@@ -1,7 +1,10 @@
-<script lang="ts"></script>
+<script lang="ts">
+    import background_video from "$lib/assets/videos/landing-page-background-video.mp4";
+</script>
 
 <div class="absolute top-0 left-0 w-full h-screen -z-10 select-none no-scroll">
-    <video class="brightness-75 absolute object-cover top-0  min-w-full min-h-screen -z-[1000] select-none" autoplay muted loop playsinline>
-        <source src="/src/lib/assets/videos/landing-page-background-video.mp4" type="video/mp4"/>
+    <video class="brightness-75 absolute object-cover top-0  min-w-full min-h-screen -z-[1000] select-none" autoplay
+           muted loop playsinline>
+        <source src={background_video} type="video/mp4"/>
     </video>
 </div>
