a29a480cb8810044846e2c7ec2d21c7c728717f4
Kishorè Shanto <tinykishore@icloud.com>
2023-10-23 11:59:25 +0600

    Fixed `app.d.ts`
    Signed-off-by: Kishorè Shanto <tinykishore@icloud.com>

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,7 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+import type {USER_ROLE} from "$lib/client/utility";
+
 declare global {
 	namespace App {
 		// interface Error {}
@@ -7,27 +9,53 @@ declare global {
 		// interface PageData {}
 		// interface Platform {}
 	}
-}
 
-interface SignInCredentials {
-	key: string
-	password: string
-}
 
-interface Post {
-	title: string
-	post : string
-	author : string
-	timestamp : number
-	likes: [],
-	viewCount : number,
-	profilePicture : string
-}
+	interface SignInCredentials {
+		key: string
+		password: string
+	}
 
-interface Credentials {
-	email: string
-	username: string
-	password_hash: string
-	password_reset_token: string
-	google_id: string
+	interface Post {
+		title: string
+		post: string
+		author: string
+		timestamp: number
+		likes: [],
+		viewCount: number,
+		profilePicture: string
+	}
+
+	interface Credentials {
+		email: string
+		username: string
+		password_hash: string
+		password_reset_token: string
+		google_id: string
+	}
+
+	interface UserObject {
+		full_name: string,
+		date_of_birth: string,
+		gender: string,
+		address: {
+			street: string,
+			city: string,
+			state: string,
+			zip: string,
+			country: string
+		},
+		phone: string,
+		occupation: string,
+		social_connections: null,
+		credentials: {
+			email: string,
+			username: string,
+			password_hash: string,
+			password_reset_token: string,
+			google_id: string
+		},
+		profile_picture: string,
+		role: USER_ROLE,
+	}
 }
