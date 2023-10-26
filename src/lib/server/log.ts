47cfef2de4cbd0b970269692d60e26c57e6cc4b3
Kishorè Shanto <tinykishore@icloud.com>
2023-10-26 09:24:54 +0600

    Refactored
    Signed-off-by: Kishorè Shanto <tinykishore@icloud.com>

--- src/lib/server/log.ts
+++ src/lib/server/log.ts
@@ -28,16 +28,16 @@ export default function consoleLog(message: string, level: LEVEL = LEVEL.INFO): 
     // Log message
     switch (level) {
         case LEVEL.INFO:
-            console.log(`${timeStamp} [ ${message} ]`);
+            console.log(`-> ${timeStamp} [ ${message} ]`);
             break;
         case LEVEL.OK:
             console.log(`\x1b[32m-> ${timeStamp} [ ${message} ] \x1b[0m`);
             break;
         case LEVEL.ERROR:
-            console.error(`\x1b[31m-!> ${timeStamp} [ ${message} ] \x1b[0m`);
+            console.error(`\x1b[31m-> ${timeStamp} [ ${message} ] \x1b[0m`);
             break;
         case LEVEL.WARN:
-            console.warn(`\x1b[33m-~> ${timeStamp} [ ${message} ] \x1b[0m`);
+            console.warn(`\x1b[33m-> ${timeStamp} [ ${message} ] \x1b[0m`);
             break;
     }
 }
