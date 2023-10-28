bf9b2feb6bf32406e1f64fc0516545b3abcf7a5d
Kishorè Shanto <tinykishore@icloud.com>
2023-10-28 17:54:54 +0600

    Refactored DashboardNavigation.svelte

--- src/lib/components/dynamicNavigations/DashboardNavigation.svelte
+++ src/lib/components/dynamicNavigations/DashboardNavigation.svelte
@@ -71,10 +71,6 @@
 					  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0
 					  3.042 1.135 5.824 3 7.938l3-2.647z"></path>
 			</svg>
-			{:else if (full_name === undefined)}
-				<div hidden>
-					<p>Loading</p>
-				</div>
 			{:else}
 				<a href="/{username}" in:fade
 				   class="flex gap-4 rounded-full align-middle border-yellow-800/20 border justify-center items-center font-semibold text-yellow-950  hover:bg-yellow-300 hover:border-yellow-800 transition-all duration-300">
