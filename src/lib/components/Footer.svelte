8c9106e86d07c516e57a5ce8ec1459af72b1f119
Kishorè Shanto <tinykishore@icloud.com>
2023-10-29 13:02:04 +0600

    Added svelte logo in footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,6 @@
+<script>
+    import icon from "$lib/assets/icons/svelte-horizontal.svg";
+</script>
 <footer class=" fixed bottom-0 w-full md:flex mx-auto py-2 px-32 z-50
 justify-between sm:justify-between align-middle items-center text-sm text-zinc-600 shadow-sm
 rounded-t-lg bg-gradient-to-l from-yellow-100 to-amber-50">
@@ -7,10 +10,18 @@ rounded-t-lg bg-gradient-to-l from-yellow-100 to-amber-50">
         <a href="/" class="hover:text-amber-700 hover:underline">Sitemap</a>
         <a href="/" class="hover:text-amber-700 hover:underline">Privacy</a>
         <a href="/" class="hover:text-amber-700 hover:underline">Your Privacy Choices</a>
+
     </div>
 
     <div class="text-gray-900 font-bold flex-row flex gap-x-6">
         <a href="/" class="hover:underline">English (US)</a>
         <a href="/" class="hover:underline">Support and Resource</a>
+        <a href="https://kit.svelte.dev/"
+           class="select-none flex gap-1 items-center justify-start align-middle font-normal text-zinc-500">
+            Made with
+            <span>
+                <img class="w-20" src={icon} alt=""/>
+            </span>
+        </a>
     </div>
 </footer>
