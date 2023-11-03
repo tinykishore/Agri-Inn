393a75f9dab886f215108f1b32e36e20f0d47ce6
Kishorè Shanto <tinykishore@icloud.com>
2023-11-03 16:41:42 +0600

    Added product_ID navbar store

--- src/lib/stores/DynamicNavigation.ts
+++ src/lib/stores/DynamicNavigation.ts
@@ -19,3 +19,11 @@ export let search_navigation = writable({
     query: "",
     search_results: {}
 });
+
+
+export let farm_productID_navigation = writable({
+    productID: "",
+    farm_name: "",
+    product_price: 0,
+    availability: ""
+});
