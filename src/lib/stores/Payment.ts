73dbceda7e2fa7f698894db49e320c3eaf2e3631
Kishorè Shanto <tinykishore@icloud.com>
2023-11-07 00:02:38 +0600

    Added payment store

---
+++ src/lib/stores/Payment.ts
@@ -0,0 +1,23 @@
+import {writable} from "svelte/store";
+
+export default writable({
+    user_id: "",
+    product_id: "",
+    farm_id: "",
+    shipping_address: {
+        street: "",
+        city: "",
+        state: "",
+        zip: "",
+        country: ""
+    },
+    payment: {
+        method: 0,
+        card_number: "",
+        card_holder: "",
+        card_expiration: "",
+        card_cvv: "",
+        bkash_number: "",
+    },
+    installment: 0
+});
