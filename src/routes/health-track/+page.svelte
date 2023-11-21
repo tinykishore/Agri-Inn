0a60a7161aeda6e69f756fe4591e66309915e3c6
musfirat018 <mhossain201018@bscse.uiu.ac.bd>
2023-11-21 23:22:39 +0600

    health track update

--- src/routes/health-track/+page.svelte
+++ src/routes/health-track/+page.svelte
@@ -7,6 +7,7 @@
     //export let data
     DynamicNavigation.set(DashboardNavigation);
 
+
     export let data;
 
     let selectedProduct = '';
@@ -36,48 +37,76 @@
 
 </script>
 
-<main class="my-24">
+
+<style>
+    .card-wrapper {
+        background-color: #FFC107;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        border-radius: 8px;
+        padding: 16px;
+    }
+
+    .section-content {
+    }
+</style>
+
+<main class="my-24 flex justify-center items-center">
     {#if healthInfo === undefined}
-        <div class="flex flex-col my-12 mx-24 gap-8">
+        <div class="flex flex-col my-12 mx-4 gap-8">
             <h1>Loading...</h1>
         </div>
     {:else}
-        <div class="grid grid-cols-3 gap-6">
-            <div class="flex flex-col gap-3 rounded-2xl w-full p-3 hover:bg-amber-200 hover:drop-shadow-sm transition-all duration-300 items-center justify-start align-middle">
-                {healthInfo._id}
-                {#each healthInfo.selling_products as product}
-                    <button value={product} on:click={() => selectedProduct = product}>
-                        {product}
-                    </button>
-                {/each}
-            </div>
-            <div class="flex flex-col gap-2">
-                {#key selectedProduct}
-                    <Temp farm_id={healthInfo._id} product_category={selectedProduct}/>
-                {/key}
-            </div>
-            <div class="flex flex-col gap-4">
-                <div class="text-lg font-bold">Vets Suggestions for Your Animals</div>
-                {#if vetsinfo}
-                    {#each vetsinfo as vet}
-                        <div class="flex gap-4 rounded-2xl w-full p-3 hover:bg-amber-200 hover:drop-shadow-sm transition-all duration-300 items-center justify-start align-middle">
-                            <img alt="" class="w-12 h-12 rounded-full" src={vet.profile_picture}/>
-                            <div class="flex-col flex w-full justify-start align-middle items-start">
-                                <div class="flex-col flex w-full justify-start align-middle items-start">
-                                    <div class="text-lg font-bold">{vet.full_name}</div>
-                                    <div class="text-sm font-medium text-zinc-500">{vet.username} &bull; {vet.phone}</div>
+        <div class="card-wrapper">
+            <div class="grid grid-cols-3 gap-6 max-w-5xl">
+                <div class="flex flex-col gap-3 rounded-2xl w-full p-3 transition-all duration-300 items-center justify-start align-middle col-span-1">
+                    <div class="section-content scrollable-section" style="max-height: 400px; overflow-y: auto;">
+                        <h2 class="text-lg font-bold mb-2">Animals in My Farm</h2>
+                        {#each healthInfo.selling_products as product}
+                            <div class="flex gap-4 rounded-2xl w-full p-3 hover:bg-amber-200 hover:drop-shadow-sm">
+                                <button on:click={() => selectedProduct = product} class="text-lg font-bold">{product}</button>
+                            </div>
+                        {/each}
+                    </div>
+                </div>
+                {#if selectedProduct !== ''}
+                    <div class="flex flex-col gap-2 col-span-1 w-full">
+                        <div class="section-content scrollable-section" style="max-height: 400px; overflow-y: auto;">
+                            <h2 class="text-lg font-bold mb-2">Animals' Health Record</h2>
+                            <Temp farm_id={healthInfo._id} product_category={selectedProduct}/>
+                        </div>
+                    </div>
+                {/if}
+                <div class="flex flex-col gap-4 col-span-1">
+                    <div class="section-content text-lg font-bold mb-2">Vets Suggestions for Your Animals</div>
+                    <div class="section-content scrollable-section" style="max-height: 400px; overflow-y: auto;">
+                        {#if vetsinfo}
+                            {#each vetsinfo as vet}
+                                <div class="flex gap-4 rounded-2xl w-full p-3 hover:bg-amber-200 hover:drop-shadow-sm transition-all duration-300 items-center justify-start align-middle">
+                                    <img alt="" class="w-12 h-12 rounded-full" src={vet.profile_picture}/>
                                     <div class="flex-col flex w-full justify-start align-middle items-start">
-                                        <button class="bg-zinc-400 mt-1.5 text-white w-fit font-bold py-1 px-2.5 rounded-full">
-                                            View Profile
-                                        </button>
-
+                                        <div class="flex-col flex w-full justify-start align-middle items-start">
+                                            <div class="text-lg font-bold">{vet.full_name}</div>
+                                            <div class="text-sm font-medium text-zinc-500">{vet.username} &bull; {vet.phone}</div>
+                                            <div class="flex-col flex w-full justify-start align-middle items-start">
+                                                <button class="bg-zinc-400 mt-1.5 text-white w-fit font-bold py-1 px-2.5 rounded-full">
+                                                    View Profile
+                                                </button>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                    {/each}
-                {/if}
+                            {/each}
+                        {/if}
+                    </div>
+                </div>
             </div>
         </div>
     {/if}
 </main>
+
+
+
+
+
+
+
--- src/routes/health-track/Temp.svelte
+++ src/routes/health-track/Temp.svelte
@@ -30,63 +30,82 @@
 </script>
 
 <style>
-	.product-container {
+	.product-category-container {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+		gap: 20px;
+		margin: 20px;
+	}
+
+	.product-category {
+		background-color: #f8f8f8;
+		border: 1px solid #ddd;
+		border-radius: 15px;
+		overflow: hidden;
 		transition: box-shadow 0.3s ease;
 	}
 
-	.product-container:hover {
-		box-shadow: inset 0 0 10px #ffc107;
+	.product-category:hover {
+		box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
 	}
 
-	.product-container:hover h1 {
-		color: #555;
+	.product-container {
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+		padding: 20px;
 	}
 
 	.section-title {
 		font-size: 1.2em;
 		font-weight: bold;
 		margin-bottom: 0.5em;
+		color: #333;
 	}
 
 	.product-details,
 	.vaccine-records,
 	.diseases {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
+		width: 100%;
+	}
+
+	.product-container h1 {
+		color: #555;
+	}
+
+	.product-details h1,
+	.vaccine-records h1,
+	.diseases h1 {
+		margin: 0;
 	}
 </style>
 
-<div class="flex flex-col gap-3 bg-amber-300/60 backdrop-blur-2xl rounded-2xl p-6 overflow-y-scroll align-middle">
-
+<div class="product-category-container">
 	{#each products as product}
-		<div class="flex gap-4 items-center align-middle bg-orange-50 rounded-2xl product-container">
-			<div class="product-details">
-				<div class="section-title">Product ID</div>
-				<h1>{product.id}</h1>
-			</div>
+		<div class="product-category">
+			<div class="product-container">
+				<div class="product-details">
+					<div class="section-title">Product ID</div>
+					<h1>{product.id}</h1>
+				</div>
 
-			<div class="vaccine-records">
-				<div class="section-title">Vaccine Records</div>
-				{#each product.vaccination_records as pvc}
-					<div class="flex flex-col gap-2">
-						<h1>{pvc.vaccine_name}</h1>
-						<h1>{pvc.date_administered}</h1>
-					</div>
-				{/each}
-			</div>
+				<div class="vaccine-records">
+					<div class="section-title">Vaccine Records</div>
+					{#each product.vaccination_records as pvc}
+						<div class="vaccine-record">
+							<h1>{pvc.vaccine_name}</h1>
+							<h1>{pvc.date_administered}</h1>
+						</div>
+					{/each}
+				</div>
 
-			<div class="diseases">
-				<div class="section-title">Diseases</div>
-				{#each product.diseases as disease}
-					<h1>{disease}</h1>
-				{/each}
+				<div class="diseases">
+					<div class="section-title">Diseases</div>
+					{#each product.diseases as disease}
+						<h1>{disease}</h1>
+					{/each}
+				</div>
 			</div>
 		</div>
 	{/each}
-
 </div>
-
-
-
